15391592f2829bcddf42ae1afe3fa0d2342e4e8c
Jennifer V Phan <jennyphan181295@gmail.com>
2019-07-24 22:22:23 +0200

    other password show/hide input

--- public/index.js
+++ public/index.js
@@ -1,20 +1,20 @@
 function strap() {
-  let checkboxes = document.getElementsByClassName("togglePwd");
-  if (checkboxes && checkboxes.length > 0) checkboxes[0].addEventListener("click", e => {
-    e.currentTarget.classList.toggle("fa-eye")
-    e.currentTarget.classList.toggle("fa-eye-slash")
-    let pwd = document.getElementById("password");
-    if (pwd) {
-      switch (pwd.getAttribute("type")) {
-        case "password":
-          pwd.setAttribute("type", "text")
-          break;
-        case "text":
-          pwd.setAttribute("type", "password")
-          break;
-      }
-    }
-  })
+    let checkboxes = document.getElementsByClassName("togglePwd");
+    if (checkboxes && checkboxes.length > 0) checkboxes[0].addEventListener("click", e => {
+        e.currentTarget.classList.toggle("fa-eye")
+        e.currentTarget.classList.toggle("fa-eye-slash")
+        let pwd = document.getElementById("password");
+        if (pwd) {
+            switch (pwd.getAttribute("type")) {
+                case "password":
+                    pwd.setAttribute("type", "text")
+                    break;
+                case "text":
+                    pwd.setAttribute("type", "password")
+                    break;
+            }
+        }
+    })
 }
 
 
