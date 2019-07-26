a6dbf27923021f2e3c3f633ead0d152a5913bce1
Niklas Eicker <git@nikl.me>
2019-07-26 09:46:20 +0200

    a bit of css cleanup

--- passport/signup.js
+++ passport/signup.js
@@ -54,7 +54,7 @@ module.exports = function (passport) {
 
   // Generates hash using bCrypt
   var createHash = function (password) {
-    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+    return bCrypt.hashSync(password, bCrypt.genSaltSync(11), null);
   }
 
 }
