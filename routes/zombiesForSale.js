835f84bbbaf16649277d5dae1be83477f8fcaa78
Jennifer V Phan <jennyphan181295@gmail.com>
2019-07-26 14:11:10 +0200

    search input for catalog page

--- routes/zombiesForSale.js
+++ routes/zombiesForSale.js
@@ -1,14 +1,42 @@
 const express = require('express');
 const router = express.Router();
-const Zombie = require('../models/zombie')
+const Zombie = require('../models/zombie');
 
 router.get('/zombiesForSale', (req, res, next) => {
     Zombie.find({})
-        .then((zombies) => {
-            res.render('zombiesForSale', { zombies });
+        .then((zombie) => {
+            res.render('zombiesForSale', { zombie });
         })
         .catch((err) => {
             res.status(500).send(err);
+        })
+})
+
+router.post('/zombiesForSale', (req, res, next) => {
+    let zombieType = req.body.zombieType;
+    let str = zombieType.toLowerCase();
+    let search = str.split('');
+    let firstLetter = search[0]
+    search.shift();
+    search.unshift(firstLetter.toUpperCase())
+    let searchStr = search.join('');
+    Zombie.find({ type: searchStr })
+        .then((zombie) => {
+            res.render('zombiesForSale', { zombie });
+        })
+        .catch((err) => {
+            res.send(err);
+        })
+})
+
+router.get('/zombieDetail', (req, res, next) => {
+    let zombieId = req.query.zombie_id;
+    Zombie.findOne({ _id: zombieId })
+        .then((zombie) => {
+            res.render('../views/partials/zombieDetail.hbs', { zombie });
+        })
+        .catch((err) => {
+            res.send(err);
         })
 })
 
