1b8dc822ce67959469de4bfbbf0876a897ad2f5d
Jennifer V Phan <jennyphan181295@gmail.com>
2019-08-01 00:08:51 +0200

    I think it's finally working!! We are almost done!

--- routes/equipGadget.js
+++ routes/equipGadget.js
@@ -3,80 +3,66 @@ const router = express.Router();
 const OwnedGadget = require('../models/ownedGadget');
 const User = require('../models/user');
 const OwnedZombie = require('../models/ownedZombie');
+const Gadget = require('../models/gadget')
 const mongoose = require('mongoose');
 
 router.get('/equip', (req, res, next) => {
     let gadgetId = req.query.gadget_id;
     let zombieId = req.query.zombie_id;
-    debugger
+    // Gadget.findOne({ _id: gadgetId })
+
+
     OwnedGadget.findOne({ _id: gadgetId })
-        .then((gadgetToAdd) => {
+        .populate('origin')
+        .then((gadgetToEquip) => {
+            console.log(gadgetToEquip)
             OwnedZombie.findOne({ _id: zombieId })
-                .populate('gadgets')
-                .then((zombie) => {
-                    let gadgetArr = zombie.gadgets;
-                    if (gadgetArr) {
-                        debugger
-                        for (var i = 0; i < gadgetArr.length; i++) {
-                            let gadgetType = gadgetToAdd.category;
-                            if (gadgetArr[i].category === gadgetType) {
-                                let alreadyEquip = gadgetArr[i]._id
-                                Gadget.findByIdAndUpdate(alreadyEquip, { attachedTo: null }, { new: true })
-                                arr.splice(i, 1);
+                .populate({
+                    path: 'gadgets',
+                    populate: { path: 'origin' }
+                })
+                .then((zombieInSes) => {
+                    // console.log(zombieInSes)
+                    let gadgetArray = zombieInSes.gadgets;
+                    if (gadgetArray) {
+                        for (var i = 0; i < gadgetArray.length; i++) {
+                            let gadgetType = gadgetToEquip.origin.category;
+                            debugger
+                            if (gadgetArray[i].origin.category === gadgetType) {
+                                let alreadyEquip = gadgetArray[i].id;
+                                OwnedGadget.findByIdAndUpdate(alreadyEquip, { $unset: { attachedTo: 1 } }, { new: true })
+                                    .then((gad) => {
+                                        console.log(gad)
+                                    })
+                                gadgetArray.splice(i, 1);
+                                OwnedZombie.findByIdAndUpdate(zombieId, { gadgets: gadgetArray }, { new: true })
                                 i--;
                             }
                         }
+                        // return gadgetArray;
+                    }
+                    let gadId = mongoose.Types.ObjectId(gadgetId)
+                    gadgetArray.push(gadId);
+                    let zombieWithNewGad = {
+                        gadgets: gadgetArray
                     }
 
-                    gadgetArr.push(mongoose.Types.ObjectId(gadgetId))
-                    let newGadget = {
-                        gadgets: gadgetArr
-                    }
-                    OwnedZombie.findByIdAndUpdate(zombieId, newGadget, {
-                            new: true
+                    OwnedZombie.findByIdAndUpdate(zombieId, zombieWithNewGad, { new: true })
+                        .populate({
+                            path: 'gadgets',
+                            populate: { path: 'origin' }
                         })
-                        .populate('gadgets')
                         .populate('origin')
-                        .then((zombie) => {
+                        .then((zombNewGad) => {
                             let newgadgetWorn = {
                                 attachedTo: mongoose.Types.ObjectId(zombieId)
                             }
-                            Gadget.findByIdAndUpdate(gadgetId, newgadgetWorn, { new: true })
-                                .then((gadget) => {
-                                    console.log(zombie)
-                                    res.redirect('/user')
-                                        // res.render('../views/profile/ownedZombieDetail', { zombie });
+                            OwnedGadget.findByIdAndUpdate(gadgetId, newgadgetWorn, { new: true })
+                                .then(() => {
+                                    res.render('../views/profile/ownedZombieDetail', { zombieNow: zombNewGad });
                                 })
                         })
-
-
                 })
-
-
-
-            // OwnedZombie.findOne({ _id: zombieId })
-            //     .populate('gadgets')
-            //     .then((zombie) => {
-            //         if (zombie.gadgets.category === "")
-            //             let gadgetArr = zombie.gadgets;
-            //         let newGad = mongoose.Types.ObjectId(gadgetId);
-            //         newGadget = {
-            //             gadgets: newGad
-            //         }
-            //         OwnedZombie.findByIdAndUpdate(zombieId, newGadget, {
-            //                 new: true
-            //             })
-            //             .populate('gadgets')
-            //             .then((zombie) => {
-            //                 let newgadgetWorn = {
-            //                     attachedTo: mongoose.Types.ObjectId(zombieId)
-            //                 }
-            //                 Gadget.findByIdAndUpdate(gadgetId, newgadgetWorn, { new: true })
-            //                     .then((gadget) => {
-            //                         console.log(zombie)
-            //                         res.render('../views/profile/ownedZombieDetail', { zombie });
-            //                     })
-            //             })
         })
 })
 
--- routes/ownedZombieDetail.js
+++ routes/ownedZombieDetail.js
@@ -9,17 +9,44 @@ router.get('/ownedZombieDetail', secured(), (req, res, next) => {
             _id: req.query.zombie_id
         })
         .populate('origin')
+        .populate({
+            path: 'gadgets',
+            populate: { path: 'origin' }
+        })
         // .populate('gadgets')
         .then((zombieNow) => {
             // find all gadgets owned by current user
             OwnedGadget.find({
                     _id: {
                         '$in': req.user.gadgetsOwned
-                    }
+                    },
+                    attachedTo: null
                 })
                 .populate('origin')
                 .then((gadgets) => {
-                    res.render('../views/profile/ownedZombieDetail.hbs', { zombieNow, gadgets })
+                    let headwear = [];
+                    let weapon = [];
+                    let clothing = [];
+                    for (var i = 0; i < gadgets.length; i++) {
+                        if (gadgets[i].origin.category === "Headwear") {
+                            let obj = gadgets[i].origin
+                            let id = gadgets[i].id
+                            obj.ownedGadId = `${id}`;
+                            debugger
+                            headwear.push(obj);
+                        } else if (gadgets[i].origin.category === "Weapon") {
+                            let obj1 = gadgets[i].origin
+                            let id1 = gadgets[i].id
+                            obj1.ownedGadId = `${id1}`;
+                            weapon.push(gadgets[i].origin);
+                        } else {
+                            clothing.push(gadgets[i].origin);
+                            let obj2 = gadgets[i].origin
+                            let id2 = gadgets[i].id
+                            obj2.ownedGadId = `${id2}`;
+                        }
+                    }
+                    res.render('../views/profile/ownedZombieDetail.hbs', { zombieNow, headwear, weapon, clothing })
                 })
         })
 })
