3287e211a03b6236a331e023294f129e4270d9f0
Niklas Eicker <git@nikl.me>
2019-07-31 10:58:23 +0200

    preliminary implementation of arena fights

--- routes/ownedZombieDetail.js
+++ routes/ownedZombieDetail.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const OwnedZombie = require('../models/ownedZombie');
+var secured = require('../lib/middleware/secured');
 
-router.get('/ownedZombieDetail', (req, res, next) => {
+router.get('/ownedZombieDetail', secured(), (req, res, next) => {
     let zombieId = req.query.zombie_id;
     OwnedZombie.findOne({ _id: zombieId })
         .populate('origin')
